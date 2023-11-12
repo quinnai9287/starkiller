65c90a2ba1fc8944a95fd6472e397246783abfdc
quinnai9287 <keira0930@gmail.com>
2023-11-13 03:42:24 +0800

    feat: add essential dev tools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'STFN | Portfolio',
+      meta: [
+        { name: 'Title', content: 'STFN | Portfolio' },
+        { name: 'description', content: 'My amazing site.' },
+      ],
+      bodyAttrs: {
+        class: 'test',
+      },
+    },
+  },
   devtools: { enabled: true },
-  css: ['@/assets/main.scss'],
+  css: ['@/assets/vendor-style.css', '@/assets/main.scss'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "@/assets/stfn/_variables.scss" as *;'
-        }
+          additionalData: '@use "@/assets/stfn/_variables.scss" as *;',
+        },
       },
     },
   },
-})
+  modules: ['@fullpage/nuxt-fullpage', '@element-plus/nuxt'],
+});
